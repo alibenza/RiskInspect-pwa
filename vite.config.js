c1059e0a4287c64065b0f856c6722d17f10c8753
alibenza <130667164+alibenza@users.noreply.github.com>
2026-02-15 12:05:07 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          charts: ['chart.js', 'react-chartjs-2']
-        }
-      }
-    }
-  }
 })
